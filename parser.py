b0781d5f13ca77ecc796d1d369135656d1cd69b9
AinaNadal <aina.nadal0gmail.com>
2025-09-08 17:12:14 +0200

    function preprocess finished

--- parser.py
+++ parser.py
@@ -62,7 +62,11 @@ def preprocess(sentence):
     and removing any word that does not contain at least one alphabetic
     character.
     """
-    raise NotImplementedError
+    # Tokenize sentence into words
+    words = nltk.word_tokenize(sentence)
+    # Convert to lowercase and filter out non-alphabetic words
+    words = [w.lower() for w in words if w.isalpha()]
+    return words
 
 
 def np_chunk(tree):
